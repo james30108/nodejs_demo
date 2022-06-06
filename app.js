d19f5f6cb6227f50320aa8a22bda33feff7145d1
james30108 <independente30108@gmail.com>
2022-06-06 16:38:49 +0700

    6-6-65

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 
 const express = require("express")
 const path = require("path")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
 const router = require("./routes/router.js")
 const app = express()
 
 app.set("views", path.join(__dirname,"views")) // กำหนด views
 app.set("view engine", "ejs") // ระบุรูปแบบ engine ในที่นี้ใช้แพ็กเกจเป็น ejs
+
+// use คือการกำหนด middleware หรือก็คือตัวกรอง request ที่ส่งเข้ามา
 app.use(express.urlencoded({extended:false})) // คำสั่งแปลงข้อมูลที่ถูกส่งมาแบบ post 
+app.use(cookieParser())
+app.use(session({secret:"mysession", resave:false, saveUninitialized:false}))
 app.use(router)
 app.use(express.static(path.join(__dirname,"public")))
 
--- routes/router.js
+++ routes/router.js
@@ -27,20 +27,57 @@ router.get("/", (req,res)=>{
     const name = "James"
     const age = 15
     const address = "<h3>เชียงใหม่</h3>"
-    const products = ["เสื้อ", "กางเกง", "หมวก"]
+    const products1 = ["เสื้อ", "กางเกง", "หมวก"]
     const products2 = [
         {name:"โน๊ตบุ้ค", price:1500, image:"images/products/product1.png"},
         {name:"เสื้อ", price:700, image:"images/products/product2.png"}
     ]
-    res.render("index", {name:name, age:age, address:address, products:products, products2:products2})
+
+    // ทำการค้นหาข้อมุลใน model Product ออกมา
+    Product.find().exec((err,doc)=>{
+        res.render("index",{products:doc, name:name, age:age, address:address, products1:products1, products2:products2})
+    }) 
 })
 
-router.get("/addForm", (req,res)=>{
-    res.render("form") // ระบุหน้าที่ต้องการให้เปิด
+router.get("/add-product", (req,res)=>{
+    
+    if (req.session.login) {
+        res.render("form") // ระบุหน้าที่ต้องการให้เปิด
+    }
+    else {
+        res.render("admin")
+    }
 })
 
 router.get("/manage", (req,res)=>{
-    res.render("manage") // ระบุหน้าที่ต้องการให้เปิด
+    
+    if (req.session.login) {
+        Product.find().exec((err,doc)=>{
+            res.render("manage",{products:doc})
+        })
+    }
+    else {
+        res.render("admin")
+    }
+    
+})
+
+router.get("/logout", (req,res)=>{
+   
+    // res.clearCookie("username")
+    // res.clearCookie("password")
+    // res.clearCookie("login")
+    req.session.destroy((err)=>{
+        res.redirect("/manage")
+    })
+    
+})
+
+router.get("/delete/:id", (req,res)=>{
+    Product.findByIdAndDelete(req.params.id, {userFindAndModify:false}).exec(err=>{
+        if(err) this.console.log(err)
+        res.redirect("/manage")
+    })
 })
 
 // ตัวรับข้อมูลจาก form แบบ get
@@ -70,5 +107,64 @@ router.post("/insert", upload.single("image"), (req,res)=>{
     //res.render("form")
 
 })
+
+router.get("/:id", (req,res)=>{
+    const product_id = req.params.id
+    Product.findOne({_id:product_id}).exec((err, doc) => { // ค้าหาสินค้าจากไอดีที่ส่งเข้ามา
+        res.render("product", {product:doc}) // ระบุหน้าที่ต้องการให้เปิด
+    }) 
+    
+})
+
+router.post("/edit", (req,res)=>{
+    const edit_id = req.body.edit_id
+    Product.findOne({_id:edit_id}).exec((err, doc) => { // ค้าหาสินค้าจากไอดีที่ส่งเข้ามา
+        res.render("edit", {product:doc}) // นำข้อมูลเดิมไปแสดงที่หน้าแก้ไขข้อมูล
+    }) 
+})
+
+router.post("/update", (req,res)=>{
+    
+    // ข้อมูลใหม่ที่ถูกส่งมาจากฟอร์มแก้ไข
+    const update_id = req.body.update_id
+    let data = {
+        name:req.body.name,
+        price:req.body.price,
+        description:req.body.description
+    }
+    Product.findByIdAndUpdate(update_id, data, {userFindAndModify:false}).exec(err=>{
+        res.redirect("/manage")
+    })
+
+})
+
+router.post("/login", (req,res)=>{
+    
+    const username = req.body.username
+    const password = req.body.password
+    const timeout  = 20000 // 10 วินาที 
+
+    if (username === "admin" && password === "123") {
+        
+        // เก้บค่าลงใน cookies
+        // res.cookie("username", username, {maxAge:timeout})
+        // res.cookie("password", password, {maxAge:timeout})
+        // res.cookie("login", true, {maxAge:timeout}) // true => login เข้าสู่ระบบแล้ว
+        
+        // สร้าง session
+        req.session.username = username
+        req.session.password = password
+        req.session.login = true
+        req.session.cookie.maxAge = timeout
+        res.redirect("/manage")
+    }
+    else {
+
+        res.render("404")
+
+    }
+})
+
+
 
 module.exports = router // ทำการ export 
